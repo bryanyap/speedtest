111508348c1e7e387eb06dfe85478dcc25bb6803
Bryan Yap <bryanyap2002@hotmail.com>
2016-04-17 13:58:21 +0800

    added constants class

---
+++ app/src/main/java/me/bryanyap/speedtest/constants/ApplicationConstants.java
@@ -0,0 +1,15 @@
+package me.bryanyap.speedtest.constants;
+
+public interface ApplicationConstants {
+    String URL_10MB = "http://www.speedtest.com.sg/test_random_10mb.zip";
+    String URL_100MB = "http://www.speedtest.com.sg/test_random_100mb.zip";
+    String URL_500MB = "http://www.speedtest.com.sg/test_random_500mb.zip";
+
+    String CHANGED = "changed";
+    String FREQUENCY = "frequency";
+
+    String FIREBASE = "https://boiling-inferno-6791.firebaseio.com/";
+    String RESULTS = "results";
+
+    int SETTINGS_CHANGED_REQUEST = 1;
+}
